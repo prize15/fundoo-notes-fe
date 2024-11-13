e861ea9e2630eacbd858a85e6ca517abb0680a7e
prize15 <abhinavaseeja2000@gmail.com>
2024-11-13 15:35:03 +0530

    [ABHINAV] Initial commit with setup of react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <>
+     <Button variant="contained">Contained</Button>
+     <span className='any'>this is text of span</span>
+    </>
+   
+
+  );
+}
+
+export default App;
